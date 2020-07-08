53d42561f7812d94fe40568eb86f5bc89720d237
Henry De la Mano <hdelamano@gmail.com>
2020-07-08 06:01:10 -0400

    ajustes a la vista 'Exchange fragment'

--- app/src/main/java/com/chinchin/ui/exchange/ExchangeFragment.kt
+++ app/src/main/java/com/chinchin/ui/exchange/ExchangeFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.chinchin.R
@@ -13,7 +16,8 @@ import kotlinx.android.synthetic.main.fragment_exchange.*
 
 class ExchangeFragment : Fragment() {
 
-    private lateinit var viewModel: ExchangeViewModel
+    //private lateinit var viewModel: ExchangeViewModel
+    private val viewModel by viewModels<ExchangeViewModel>()
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -33,7 +37,7 @@ class ExchangeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ExchangeViewModel::class.java)
+        //viewModel = ViewModelProvider(this).get(ExchangeViewModel::class.java)
 
         resultadoTextView.text = getString(R.string.exchange_fragment_resultado_textview_no_value)
 
@@ -41,6 +45,26 @@ class ExchangeFragment : Fragment() {
             resultadoTextView.text = result.toString()
         }
         viewModel.getResultado().observe(viewLifecycleOwner, resultadoObserver)
+
+        // Setting Adapter to Spinners
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.divisas_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            divisaSpinner.adapter = adapter
+        }
+        divisaSpinner.setSelection(0)  // seleccion por default
+        divisaSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // do nothing
+            }
+
+            override fun onItemSelected( parent: AdapterView<*>?, view: View?, position: Int, id: Long ) {
+                //TODO consultar el endpoint para obtener el valor correspondiente a la divisa seleccionada
+            }
+        }
 
         convertButton.setOnClickListener {
             if (montoText.text.isNotEmpty()) {
